1be271cca15184d5ef9d2da7f43259f19d9db0f1
Petros Zantis <65984866+PetrosZantis13@users.noreply.github.com>
2021-12-08 20:06:53 +0000

    Checkpoint save

--- hydra/plots.py
+++ hydra/plots.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import errormodel_myedits as em
+import errormodel_forplots as em
+#import errormodel_myedits as em   # for backup
 
 plt.rcParams['figure.figsize'] = (8, 6)
 plt.rcParams['font.size'] = 11
@@ -27,13 +28,13 @@ chi = 0
 SA = 10**-12
 nbar = 0.1
 sym_fluc = 0
+dx = 1e-9
 
 def plot_errors():
 
-    err_h, err_d, err_t, err_o, err_a = em.compute_total_errors(NU_C_LIST, Om, dzB, nuSE, SBa, SV, NuXY,
-                                                    pulse_shaping = pulse_shaping, g_factor = g_factor,
-                                                    vib_mode = vib_mode, chi = chi, dx = 1e-9, SA = SA,
-                                                    nbar = nbar, sym_fluc = sym_fluc)
+    err_h, err_d, err_t, err_o, err_a = em.compute_total_errors(
+        NU_C_LIST, Om, dzB, nuSE, SBa, SV, NuXY, pulse_shaping, 
+        g_factor, vib_mode, chi, dx, SA, nbar, sym_fluc)
     
     err_tot = err_h + err_d + err_t + err_a
     
@@ -51,4 +52,41 @@ def plot_errors():
     plt.legend()
     plt.show()
 
-plot_errors()
+#plot_errors()
+
+'''
+JUST TO TEST THE NEW FUNCTION, RESTRUCTURE LATER
+'''
+
+nu_c = 300*KHZ
+nuSE_LIST = np.linspace(0, 10**-4, 101)
+
+def plot_errors2():
+
+    err_h, err_d, err_t, err_o, err_a = em.compute_total_errors(
+        nu_c, Om, dzB, nuSE_LIST, SBa, SV, NuXY, pulse_shaping, 
+        g_factor, vib_mode, chi, dx, SA, nbar, sym_fluc)
+    
+    err_tot = err_h + err_d + err_t + err_a
+    
+    plt.plot(nuSE_LIST, err_tot, label = "Total Errors", color = 'b')
+    plt.plot(nuSE_LIST, err_h, label = "Heating", color = 'r')
+    plt.plot(nuSE_LIST, err_d, label = "Decoherence", color = 'k')
+    plt.plot(nuSE_LIST, err_t, label = "Kerr", color = 'm')
+    plt.plot(nuSE_LIST, err_a, label = "Amplitude", color = 'g')
+    
+    plt.title("Errors (Infidelity) over Secular Frequency")
+    plt.xlabel("COM frequency (kHz)")
+    plt.ylabel("Infidelity")
+    plt.yscale("log")
+    plt.ylim(10**-3, 10**0)
+    plt.legend()
+    plt.show()
+
+plot_errors2()
+
+# TRY IT AGAINST ALL VARIABLES
+
+
+
+
--- hydra/testing.py
+++ hydra/testing.py
@@ -1,14 +1,34 @@
-test= 3/ (2/10)
-
-
-print(test)
-
-
-test= 3/2/10
-
-
-print(test)
-
+import numpy as np
 print("from eclipse")
 
 print("from atom")
+
+
+def foo(*args):
+    
+    params = list(args)
+    list_idx = 0
+    
+    #print( [type(args[i]) for i in range(0,len(args))] )
+    for i in range(0,len(params)):
+#         print(type(args[i]))
+#         print(type(args[i]) == np.ndarray)
+        if(type(params[i]) == np.ndarray):
+            list_idx = i
+            print("List is in position: " + str(list_idx))
+            varlist = params[i]    
+    
+    
+    for var in varlist:   # do with zip
+        params[list_idx] = var 
+        a, b, c, d, e = params
+                
+        print(a,b,c,d,e)
+        
+    
+    
+    print("end\n")
+
+foo(3.0, 45, np.linspace(0,3,4), 4, 5)
+
+foo(3.0, 45, 4, 5, np.linspace(0,3,4))
